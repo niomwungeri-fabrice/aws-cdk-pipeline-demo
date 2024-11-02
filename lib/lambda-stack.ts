11281813228c89c46dee227cbbc028ab781b0b80
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2024-11-02 11:40:17 -0600

    fix lamdbda naming issue

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import * as path from 'path';
 
 export class MyLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
@@ -8,7 +9,7 @@ export class MyLambdaStack extends cdk.Stack {
     new Function(this, 'LambdaFunction', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'handler.handler',
-      code: Code.fromAsset("lambda"),
+      code: Code.fromAsset(path.join(__dirname, "lambda")),
       environment: { "stageName": stageName }
     })
   }
--- lib/stages.ts
+++ lib/stages.ts
@@ -5,6 +5,6 @@ import { MyLambdaStack } from './lambda-stack';
 export class MyPipelineStage extends cdk.Stage {
   constructor(scope: Construct, stageName: string, props?: cdk.StageProps) {
     super(scope, stageName, props)
-    const lambdaStack = new MyLambdaStack(this, 'MyLambdaStack', stageName)
+    const lambdaStack = new MyLambdaStack(this, 'LambdaStack', stageName)
   }
 }
